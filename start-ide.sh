6e06f048b9a61bc708c534ab84a52833add3d1f6
Michael R. Furman <n6il@ocs.net>
2021-12-07 23:56:25 -0800

    fix the start script to use correct rom

--- start-ide.sh
+++ start-ide.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 xroar \
 	-machine coco3 \
-	-cart-rom hdbdw3bc3.rom \
 	-tv-input rgb \
 	-machine-cart ide \
+	-cart-rom hdblba.rom \
 	-load-hd0 68IDE.ide \
 
 # DO NOT REMOVE BLANK LINE ABOVE
